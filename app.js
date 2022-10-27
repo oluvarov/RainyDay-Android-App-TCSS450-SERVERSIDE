db125cbc71a26da8c391742ff6d5c8fed3e87c33
Oleg <52720934+ondip@users.noreply.github.com>
2022-10-27 13:51:58 -0700

    Initial Node.JS project setup - Oleg

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');  
+const app = express();  
+const port = 3000;  
+app.get('/', (req, res) => res.send('Welcome To Nodejs!'));  
+app.listen(3000, () => console.log(`Example app listening on port ${port}!`)); 
