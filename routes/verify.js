96ce6851e80498243c2b663a29b3d2184abcc3b9
Oleg <52720934+ondip@users.noreply.github.com>
2022-11-08 21:57:10 -0800

    Added bad request handling for email verification

--- routes/verify.js
+++ routes/verify.js
@@ -29,6 +29,9 @@ const router = express.Router()
  */ 
 router.get('/', function(req, res, next){
     const code = req.query.code;
+    if (code == 1) {
+        res.status(400).send('🚫Bad request!') 
+    }
     
     const theQuery = 'SELECT MemberID, Email, Verification FROM MEMBERS WHERE Verification = $1'
     const values = [code]
