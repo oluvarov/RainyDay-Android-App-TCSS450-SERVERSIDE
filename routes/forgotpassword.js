e410caa5190b01a64fa1427cf1a883490695924a
jessiedejesus <jdejesus@uw.edu>
2022-11-13 11:02:39 -0800

    update forgotpassword

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -16,57 +16,43 @@ const sendEmail = require('../utilities').sendEmail
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-        const email = req.body.email
-        req.memberid = req.rows[0].memberid
-        const memberid = req.memberid
 
-        if(isStringProvided(email)) {
-            res.status(201).send( {
-                success: true,
-                message: email,
-                memberid: memberid
-        })
+router.get("/", (req, res) => {
+    const email = req.body.email
+    if(isStringProvided(email)) {
+        const memberid = req.memberid;
+        const theQuery = `SELECT saltedhash, salt, Credentials.memberid FROM Credentials
+                          INNER JOIN Members ON
+                          Credentials.memberid=Members.memberid 
+                          WHERE Members.email=$1`
+        const values = [email]
+        pool.query(theQuery, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    res.status(404).send({
+                        message: "User Information not found",
+                        address: address,
+                        theQuery: theQuery
+                    })
+                }  else {
+                    let salt = generateSalt(32)
+                    let newSaltedHash = generateHash("randomPassword", salt) //hash for new password
+                    const theQuery = 'UPDATE CREDENTIALS SET saltedhash = $1, salt = $2 WHERE email = $3'
+                    const values = [newSaltedHash, salt, email]
+                    pool.query(theQuery, values)
+                            .then(result => {
+                                res.status(201).send( {
+                                    success: true,
+                                    message: "Temporary password created"
+                                })
+                                sendEmail("tcss450chat@gmail.com", request.body.email, "New Temporary Password", 'Your new password: ' + newPassword)
+                    })
+                    
+                }
+            })
+            return 
     }
 })
-
-
-// router.get("/", (req, res) => {
-//     const email = req.body.email
-//     if(isStringProvided(email)) {
-//         const memberid = req.memberid;
-//         const theQuery = `SELECT saltedhash, salt, Credentials.memberid FROM Credentials
-//                           INNER JOIN Members ON
-//                           Credentials.memberid=Members.memberid 
-//                           WHERE Members.memberid=$1`
-//         const values = [memberid]
-//         pool.query(theQuery, values)
-//             .then(result => {
-//                 if (result.rowCount == 0) {
-//                     res.status(404).send({
-//                         message: "User Information not found",
-//                         address: address,
-//                         theQuery: theQuery
-//                     })
-//                 }  else {
-//                     let salt = generateSalt(32)
-//                     let newSaltedHash = generateHash("randomPassword", salt) //hash for new password
-//                     const theQuery = 'UPDATE CREDENTIALS SET saltedhash = $1, salt = $2 WHERE MemberID = $3'
-//                     const values = [newSaltedHash, salt, memberid]
-//                     pool.query(theQuery, values)
-//                             .then(result => {
-//                                 res.status(201).send( {
-//                                     success: true,
-//                                     message: "Temporary password created"
-//                                 })
-//                                 sendEmail("tcss450chat@gmail.com", request.body.email, "New Temporary Password", 'Your new password: ' + newPassword)
-//                     })
-                    
-//                 }
-//             })
-//             return 
-//     }
-// })
 
 module.exports = router
 
