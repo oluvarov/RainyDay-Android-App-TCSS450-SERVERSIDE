523ffc369ad134e14d31f079b26c8b4c0bb70477
jessiedejesus <jdejesus@uw.edu>
2022-11-13 06:13:38 -0800

    changes to user.js for password update. corrected if statement

--- routes/user.js
+++ routes/user.js
@@ -90,11 +90,11 @@ router.post('/update/name', function(req, res, next){
 router.post('/update/password', (req, res, next) => {
 
     if (!isStringProvided(req.header("oldPassword")) || !isStringProvided(req.header("newPassword"))) {
-        res.status(400).send('incorrect information')
+        res.status(400).send('Missing Information')
         return
     } else 
      if(req.header("oldPassword") === req.header("newPassword")){
-        res.status(400).send('bad request: old password cannot be the same as new ')
+        res.status(400).send('Bad request: old password cannot be the same as new ')
         return
     } else {
         req.memberid = req.decoded.memberid;
