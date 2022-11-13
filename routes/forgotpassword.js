050be78d09ceb53bbb81710c0c73ce4d3422812b
jessiedejesus <jdejesus@uw.edu>
2022-11-13 12:02:44 -0800

    update forgotpassword

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -22,7 +22,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 
-    const memberid = req.decoded.memberid;
+    //const memberid = req.decoded.memberid;
     const email = req.body.email
 
 
