45a375f853ad3d55c8a77b5dfc5ddd5e697abdbb
Oleg <52720934+ondip@users.noreply.github.com>
2022-11-18 21:27:37 -0800

    Added weather API

---
+++ routes/weather.js
@@ -0,0 +1,115 @@
+//express is the framework we're going to use to handle requests
+const { response } = require('express')
+const { request } = require('express')
+const express = require('express') 
+const pool = require('../utilities').pool
+const http = require('http')
+
+const url = require('url');
+const querystring = require('querystring');
+const { nextTick } = require('process');
+
+const router = express.Router()
+
+
+/**
+ * @api {get} /verification Request to verify email
+ * @apiName getEmailVerification
+ * @apiGroup Verification
+ * @apiParam code
+ * 
+ * @apiSuccess {boolean} success true when the email is found and code matched.
+ * @apiSuccess {String} message "✅Your email was successfully verified!"
+ * 
+ *  * @apiSuccessExample {html} Success-Response:
+ *     HTTP/1.1 200 OK
+ *       <h3>✅Your email was successfully verified!</h3>
+ * 
+ * @apiError (404: User Not Found) {String} message "User not found"
+ * 
+ */ 
+router.get('/current', function(req, res, next){
+    const ip = req.headers.ip;
+    if (ip.length == 0) {
+        res.status(400).send('🚫Bad request!') 
+    }
+
+    let url = 'http://ip-api.com/json/'
+
+    http.get(url + ip, response => { //collecting response
+        let rawData = ''
+        response.on('data', chunk => {
+            rawData += chunk
+        })
+    
+        response.on('end', () => {
+            const parsedData = JSON.parse(rawData)
+            req.zip = parsedData.zip;
+            next()
+        })
+    })
+    
+  }, (req, res, next) => {
+
+    let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + req.zip + '&appid=4087482a4cfc4a57ffbdd797365dd918&units=metric'
+
+    http.get(url, response => {
+        let rawData = ''
+        response.on('data', chunk => {
+            rawData += chunk
+        })
+    
+        response.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            req.weather = parsedData
+            next()
+        })
+
+    })
+  }, (req,res) => {
+    res.status(200).send(req.weather)
+  })
+
+router.get('/forecast', function(req, res, next){
+    const ip = req.headers.ip;
+    if (ip.length == 0) {
+        res.status(400).send('🚫Bad request!') 
+    }
+
+    let url = 'http://ip-api.com/json/'
+
+    http.get(url + ip, response => { //collecting response
+        let rawData = ''
+        response.on('data', chunk => {
+            rawData += chunk
+        })
+    
+        response.on('end', () => {
+            const parsedData = JSON.parse(rawData)
+            req.zip = parsedData.zip;
+            next()
+        })
+    })
+    
+  }, (req, res, next) => {
+
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?zip=' + req.zip + '&appid=4087482a4cfc4a57ffbdd797365dd918&cnt=5'
+
+    http.get(url, response => {
+        let rawData = ''
+        response.on('data', chunk => {
+            rawData += chunk
+        })
+    
+        response.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            req.weather = parsedData
+            next()
+        })
+
+    })
+  }, (req,res) => {
+    res.status(200).send(req.weather)
+  })
+
+module.exports = router 
