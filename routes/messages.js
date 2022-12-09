7530031c199479a05330389161a8e5c844e9b169
Oleg <52720934+ondip@users.noreply.github.com>
2022-12-08 22:10:11 -0800

    updated messages

--- routes/messages.js
+++ routes/messages.js
@@ -98,14 +98,14 @@ router.post("/", (request, response, next) => {
     //add the message to the database
     let insert = `INSERT INTO Messages(ChatId, Message, MemberId)
                   VALUES($1, $2, $3) 
-                  RETURNING PrimaryKey AS MessageId, ChatId, Message, MemberId AS email, TimeStamp`
+                  RETURNING PrimaryKey AS MessageId, ChatId, Message, MemberId AS email, TimeStamp,  MemberId AS FirstName, MemberId AS LastName`
     let values = [request.body.chatId, request.body.message, request.decoded.memberid]
     pool.query(insert, values)
         .then(result => {
             if (result.rowCount == 1) {
                 //insertion success. Attach the message to the Response obj
-                result.rows[0].firstName = request.decoded.firstName
-                result.rows[0].lastName = request.decoded.lastName
+                //result.rows[0].firstName = request.decoded.firstName
+                //result.rows[0].lastName = request.decoded.lastName
                 response.message = result.rows[0]
                 response.message.email = request.decoded.email
                 response.message.firstName = request.decoded.firstName
